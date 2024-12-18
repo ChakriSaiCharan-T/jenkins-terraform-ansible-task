7f9ec6868c0a057c4599866bd93d0bff4315fdad
narendarmuchunuri <narendarmuchunuri@outlook.com>
2024-12-18 17:56:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "backend" { #ubuntu.yaml NETADATA
   ami                    = "ami-0e2c8caa4b6378d8c"
   instance_type          = "t2.micro" 
   key_name               = "narenadr"
-  vpc_security_group_ids = ["sg-0806a0e627024f14d"]
+  
   tags = {
     Name = "u21.local"
   }
@@ -26,7 +26,7 @@ resource "aws_instance" "frontend" { #amazon-playbook.yaml NGINX
   ami                    = "ami-01816d07b1128cd2d"
   instance_type          = "t2.micro"
   key_name               = "narendar"
-  vpc_security_group_ids = ["sg-0806a0e627024f14d"]
+  
   tags = {
     Name = "c8.local"
   }
